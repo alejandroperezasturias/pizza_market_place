45065c2ce32a414dcc25f31a0d29b1d0b2dc4741
Alejandro Perez <alexroperez4@gmail.com>
2021-02-02 16:47:45 +0100

    lazy

--- src/pages/Pizzas.js
+++ src/pages/Pizzas.js
@@ -1,18 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, lazy, Suspense } from 'react';
 import { AnimatePresence } from 'framer-motion';
 import PizzasFavourites from '../Components/PizzasFavourties';
 import PizzasSecondSection from '../Components/PizzasSecondSection';
-import PizzaDetail from '../Components/PizzaDetail';
+// import PizzaDetail from '../Components/PizzaDetail';
 import { PizzaContext } from '../App.js';
+const PizzaDetail = React.lazy(() => import('../Components/PizzaDetail'));
 
-
+// {toogleSideBar && (
+// 	<Suspense fallback={<div>Loading Sidebar...</div>}>
+// 		<Trolly />
+// 	</Suspense>
+// )}
 export default function Pizza() {
 	const { itemModifier } = useContext(PizzaContext);
 
 	return (
 		<div>
-			<AnimatePresence >
-				{itemModifier && <PizzaDetail />}
+			<AnimatePresence>
+				{itemModifier && (
+					<Suspense fallback={<div>Loading Modifier...</div>}>
+						<PizzaDetail />
+					</Suspense>
+				)}
 			</AnimatePresence>
 			<PizzasFavourites />
 			<PizzasSecondSection />
